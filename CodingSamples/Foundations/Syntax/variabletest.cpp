a4f166b8de55bd239da974f931608c5ad069c5ff
nileshshirkemet <43514175+nileshshirkemet@users.noreply.github.com>
2026-09-11 16:22:12 +0530

    Add files via upload

--- CodingSamples/Foundations/Syntax/variabletest.cpp
+++ CodingSamples/Foundations/Syntax/variabletest.cpp
@@ -1,7 +1,18 @@
 #include <cstdio>
 
+float rate = 0.85; //global variable (file scope - value in data-section)
+
 int main(void) 
 {
-    puts("Hello World!");
-    return 9;
+    float width = 0; //local variable (block scope - value on stack)
+    float height = 0;
+    printf("Dimensions of Banner: ");
+    scanf("%f%f", &width, &height);
+
+    short int count = 0;
+    printf("Number of Banners: ");
+    scanf("%hd", &count);
+
+    double amount = count * width * height * rate;
+    printf("Total Payment = %.2lf\n", amount);
 }
